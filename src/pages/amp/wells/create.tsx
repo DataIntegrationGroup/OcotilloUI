bcbaf9b87eb8d578163db0a78a7862030aa92cf5
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2025-02-11 11:26:50 -0600

    [wells/create] Add dynamic design

--- src/pages/amp/wells/create.tsx
+++ src/pages/amp/wells/create.tsx
@@ -1,15 +1,21 @@
 import type { HttpError } from "@refinedev/core";
 import { Create } from "@refinedev/mui";
-import Box from "@mui/material/Box";
-import TextField from "@mui/material/TextField";
 import { useForm } from "@refinedev/react-hook-form";
 import type { IWellForm } from "../../../interfaces/amp";
 import { Nullable } from "../../../interfaces";
 import { DateTimePicker, LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { Divider, Stack, Typography } from "@mui/material";
 import { useState } from "react";
-import { Dayjs } from "dayjs";
+import {
+  TextField,
+  Box,
+  Grid,
+  Typography,
+  Divider,
+  useMediaQuery,
+  useTheme
+} from "@mui/material";
+import type { Dayjs } from "dayjs";
 
 export const WellCreate: React.FC = () => {
   const {
@@ -20,20 +26,18 @@ export const WellCreate: React.FC = () => {
 
   const [dateTime, setDateTime] = useState<Dayjs | null>(null);
 
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
+
   return (
     <Create saveButtonProps={saveButtonProps}>
-      <Box
-        component="form"
-        autoComplete="off"
-      >
-        <Stack spacing={2} divider={<Divider orientation="horizontal" flexItem />}>
-          <Stack spacing={2}>
-            <Stack direction="row" spacing={2}>
+      <Box component="form" autoComplete="off">
+        <Grid container spacing={2}>
+          <Grid container item spacing={2} xs={12} md={6} direction={isMobile ? "column" : "row"}>
+            <Grid item xs={12} sm={6}>
               <TextField
-                {...register("WellID", {
-                  required: "This field is required",
-                })}
-                required={true}
+                {...register("WellID", { required: "This field is required" })}
+                required
                 error={!!errors.WellID}
                 helperText={errors.WellID?.message}
                 fullWidth
@@ -41,6 +45,8 @@ export const WellCreate: React.FC = () => {
                 name="WellID"
                 autoFocus
               />
+            </Grid>
+            <Grid item xs={12} sm={6}>
               <TextField
                 {...register("SiteName")}
                 error={!!errors.SiteName}
@@ -48,35 +54,36 @@ export const WellCreate: React.FC = () => {
                 fullWidth
                 label="Site Name"
                 name="SiteName"
-                autoFocus
               />
-            </Stack>
-            <Stack direction="row" spacing={2}>
+            </Grid>
+          </Grid>
+          <Grid container item spacing={2} xs={12} md={6} direction={isMobile ? "column" : "row"}>
+            <Grid item xs={12} sm={6}>
               <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <DateTimePicker
-                  {...register("DateTime", {
-                    required: "This field is required",
-                  })}
+                  sx={{ width: '100%' }}
                   label="Date Time"
                   value={dateTime}
                   onChange={(newDateTime) => setDateTime(newDateTime)}
                 />
               </LocalizationProvider>
+            </Grid>
+            <Grid item xs={12} sm={6}>
               <TextField
                 {...register("FieldStaff")}
                 error={!!errors.FieldStaff}
                 helperText={errors.FieldStaff?.message}
-                label="Field Staff"
-                name="Field Staff"
                 fullWidth
-                autoFocus
+                label="Field Staff"
+                name="FieldStaff"
               />
-            </Stack>
-          </Stack>
-          <Stack spacing={2}>
-            <Typography variant="h3" component="h3">
-              Owner Data
-            </Typography>
+            </Grid>
+          </Grid>
+          <Grid item xs={12}>
+            <Divider />
+            <Typography variant="h6">Owner Data</Typography>
+          </Grid>
+          <Grid item xs={12}>
             <TextField
               {...register("Owner.Name")}
               error={!!errors.Owner?.Name}
@@ -84,14 +91,13 @@ export const WellCreate: React.FC = () => {
               fullWidth
               label="Owner Name"
               name="Owner Name"
-              autoFocus
-            />          </Stack>
-          <Stack spacing={2}>
-            <Typography variant="h3" component="h3">
-              Well Data
-            </Typography>
-          </Stack>
-        </Stack>
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Divider />
+            <Typography variant="h6">Well Data</Typography>
+          </Grid>
+        </Grid>
       </Box>
     </Create>
   );
