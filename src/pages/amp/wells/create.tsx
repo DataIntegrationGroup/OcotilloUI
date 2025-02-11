93ce4b7e16ed0a71054f48ea4f9b45f96a2c0052
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2025-02-10 18:24:21 -0600

    [wells/create] Rm used importants

--- src/pages/amp/wells/create.tsx
+++ src/pages/amp/wells/create.tsx
@@ -1,26 +1,17 @@
 import type { HttpError } from "@refinedev/core";
-import { Create, useAutocomplete } from "@refinedev/mui";
+import { Create } from "@refinedev/mui";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
 import { useForm } from "@refinedev/react-hook-form";
-
-import { Controller } from "react-hook-form";
-
 import type { IWellForm } from "../../../interfaces/amp";
-import {ICategory, Nullable} from "../../../interfaces";
+import { Nullable } from "../../../interfaces";
 
 export const WellCreate: React.FC = () => {
   const {
     saveButtonProps,
     register,
-    control,
     formState: { errors },
   } = useForm<IWellForm, HttpError, Nullable<IWellForm>>();
-
-  const { autocompleteProps } = useAutocomplete<ICategory>({
-    resource: "categories",
-  });
 
   return (
     <Create saveButtonProps={saveButtonProps}>
@@ -43,91 +34,15 @@ export const WellCreate: React.FC = () => {
         />
         <TextField
           {...register("SiteName", {
-              required: "This field is required",
+            required: "This field is required",
           })}
           error={!!errors.SiteName}
           helperText={errors.SiteName?.message}
           margin="normal"
           fullWidth
           label="Site Name"
-          name="Site Name"
+          name="SiteName"
           autoFocus
-        />
-        <Controller
-          control={control}
-          name="status"
-          rules={{ required: "This field is required" }}
-          // eslint-disable-next-line
-          defaultValue={null as any}
-          render={({ field }) => (
-            <Autocomplete<IStatus>
-              options={["published", "draft", "rejected"]}
-              {...field}
-              onChange={(_, value) => {
-                field.onChange(value);
-              }}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  label="Status"
-                  margin="normal"
-                  variant="outlined"
-                  error={!!errors.status}
-                  helperText={errors.status?.message}
-                  required
-                />
-              )}
-            />
-          )}
-        />
-        <Controller
-          control={control}
-          name="category"
-          rules={{ required: "This field is required" }}
-          // eslint-disable-next-line
-          defaultValue={null as any}
-          render={({ field }) => (
-            <Autocomplete
-              {...autocompleteProps}
-              {...field}
-              onChange={(_, value) => {
-                field.onChange(value);
-              }}
-              getOptionLabel={(item) => {
-                return (
-                  autocompleteProps?.options?.find(
-                    (p) => p?.id?.toString() === item?.id?.toString(),
-                  )?.title ?? ""
-                );
-              }}
-              isOptionEqualToValue={(option, value) =>
-                value === undefined ||
-                option?.id?.toString() === (value?.id ?? value)?.toString()
-              }
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  label="Category"
-                  margin="normal"
-                  variant="outlined"
-                  error={!!errors.category}
-                  helperText={errors.category?.message}
-                  required
-                />
-              )}
-            />
-          )}
-        />
-        <TextField
-          {...register("content", {
-            required: "This field is required",
-          })}
-          error={!!errors.content}
-          helperText={errors.content?.message}
-          margin="normal"
-          label="Content"
-          multiline
-          rows={4}
         />
       </Box>
     </Create>
