36dc894726742d3ac9ff73012fe250b910cfefc5
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2025-03-04 14:12:20 -0600

    [wellinventoryform] Complete owner fields

--- src/pages/amp/wellinventoryform/index.tsx
+++ src/pages/amp/wellinventoryform/index.tsx
@@ -1,42 +1,56 @@
+import { useState } from "react";
 import { useForm } from "@refinedev/react-hook-form";
 import { IWellInventoryForm } from "@/interfaces/amp";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { WellInventorySchema, SchemaDefaults } from "./well_inventory.schema";
 import {
   Box,
+  Button,
   Card,
   CardContent,
   CardHeader,
-  Grid,
   TextField,
   Typography,
 } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 import {
-  ControlledDateTimePicker,
   ControlledEmailField,
+  ControlledSelectField,
   ControlledTextField,
 } from "@/components";
-import { useTheme, useMediaQuery } from "@mui/material";
+import { useTheme } from "@mui/material";
 import { ControlledPhoneField } from "@/components/ControlledPhoneField";
+import { ControlledOSMAddressAutocomplete } from "@/components/ControlledOSMAddressAutocomplete";
 
 export const WellInventoryForm = () => {
   const theme = useTheme();
-  const isSmBreakpoint = useMediaQuery(theme.breakpoints.down("sm"));
-  const isMdBreakpoint = useMediaQuery(theme.breakpoints.down("md"));
-  const isLgBreakpoint = useMediaQuery(theme.breakpoints.down("lg"));
 
-  const { register, formState, control, handleSubmit } =
-    useForm<IWellInventoryForm>({
-      defaultValues: SchemaDefaults,
-      resolver: yupResolver(WellInventorySchema),
-      mode: "onTouched",
-    });
+  const [_, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [zip, setZip] = useState("");
+
+  const {
+    refineCore: { onFinish },
+    formState: { errors },
+    control,
+    handleSubmit,
+    reset,
+  } = useForm<IWellInventoryForm>({
+    defaultValues: SchemaDefaults,
+    resolver: yupResolver(WellInventorySchema),
+    mode: "onTouched",
+  });
 
   return (
     <Card>
       <CardHeader title="Well Inventory Form" />
-      <CardContent sx={{ padding: "2rem" }}>
-        <Box component="form" autoComplete="off">
+      <CardContent sx={{ padding: "2.5rem" }}>
+        <Box
+          component="form"
+          autoComplete="off"
+          onSubmit={handleSubmit(onFinish)}
+        >
           <Grid
             container
             spacing={2}
@@ -49,219 +63,305 @@ export const WellInventoryForm = () => {
           >
             <Grid
               container
-              item
-              spacing={2}
-              xs={12}
+              sx={{ width: "100%" }}
               direction={{ xs: "column", sm: "row" }}
             >
-              <Grid item xs={12}>
+              <Grid size={12}>
                 <Typography variant="h2">Project</Typography>
               </Grid>{" "}
-              <Grid item xs={12} sm={6} lg={4}>
-                <ControlledTextField
+              <Grid size={{ xs: 12, sm: 6, xl: 3 }}>
+                <ControlledSelectField
+                  label="PointId Prefix"
+                  control={control}
+                  name="project.pointid_prefix"
+                  options={[
+                    { value: 1, label: "One" },
+                    { value: 2, label: "Two" },
+                    { value: 3, label: "Three" },
+                  ]}
+                  errorMessage={errors.project?.pointid_prefix?.message}
+                />
+              </Grid>{" "}
+              <Grid size={{ xs: 12, sm: 6, xl: 3 }}>
+                <ControlledSelectField
                   label="Project Name"
-                  fullWidth
                   control={control}
-                  type="text"
                   name="project.project"
-                />
-              </Grid>{" "}
-              <Grid
-                item
-                xs={12}
-                sm={6}
-                lg={4}
-                sx={{ paddingLeft: isSmBreakpoint ? "" : "1rem !important" }}
-              >
-                <ControlledDateTimePicker
-                  label="Date Time"
-                  sx={{ width: "100%" }}
-                  control={control}
-                  name="project.date_time"
-                />
-              </Grid>{" "}
-              <Grid
-                item
-                xs={12}
-                lg={4}
-                sx={{ paddingLeft: isLgBreakpoint ? "" : "1rem !important" }}
-              >
-                <ControlledTextField
-                  label="Field Staff"
-                  fullWidth
-                  control={control}
-                  type="text"
-                  name="project.field_staff"
+                  options={[
+                    { value: 1, label: "One" },
+                    { value: 2, label: "Two" },
+                    { value: 3, label: "Three" },
+                  ]}
+                  errorMessage={errors.project?.project?.message}
                 />
               </Grid>{" "}
             </Grid>{" "}
-            <Grid
-              container
-              item
-              spacing={2}
-              xs={12}
-              direction={{ xs: "column", sm: "row" }}
-            >
-              <Grid item xs={12}>
+            <Grid container spacing={2} direction={{ xs: "column", sm: "row" }}>
+              <Grid size={12}>
                 <Typography variant="h2">Owner Data</Typography>
               </Grid>
-              <Grid
-                container
-                item
-                spacing={2}
-                xs={12}
-                sx={{
-                  marginLeft: "0rem !important",
-                  marginRight: "0rem !important",
-                }}
-                direction={{ xs: "column", sm: "row" }}
-              >
-                <Grid item xs={12} sm={6} lg={3}>
+              <Grid size={12}>
+                <Grid size={{ xs: 12, md: 6 }}>
                   <ControlledTextField
-                    label="First Name"
+                    label="Owner Key"
                     fullWidth
                     control={control}
                     type="text"
-                    name="owner.first_name"
-                  />
-                </Grid>{" "}
-                <Grid
-                  item
-                  xs={12}
-                  sm={6}
-                  lg={3}
-                  sx={{ paddingLeft: isSmBreakpoint ? "" : "1rem !important" }}
-                >
-                  <ControlledTextField
-                    label="Last Name"
-                    fullWidth
-                    control={control}
-                    type="text"
-                    name="owner.last_name"
-                  />
-                </Grid>{" "}
-                <Grid
-                  item
-                  xs={12}
-                  sm={6}
-                  lg={3}
-                  sx={{ paddingLeft: isLgBreakpoint ? "" : "1rem !important" }}
-                >
-                  <ControlledTextField
-                    label="First Name (Secondary)"
-                    fullWidth
-                    control={control}
-                    type="text"
-                    name="owner.second_first_name"
-                  />
-                </Grid>{" "}
-                <Grid
-                  item
-                  xs={12}
-                  sm={6}
-                  lg={3}
-                  sx={{ paddingLeft: isSmBreakpoint ? "" : "1rem !important" }}
-                >
-                  <ControlledTextField
-                    label="Last Name (Secondary)"
-                    fullWidth
-                    control={control}
-                    type="text"
-                    name="owner.second_last_name"
+                    name="owner.owner_key"
+                    errorMessage={errors.owner?.owner_key?.message}
                   />
                 </Grid>{" "}
               </Grid>{" "}
+              <Grid size={{ xs: 12, sm: 6, xl: 3 }}>
+                <ControlledTextField
+                  label="First Name"
+                  fullWidth
+                  control={control}
+                  type="text"
+                  name="owner.first_name"
+                  errorMessage={errors.owner?.first_name?.message}
+                />
+              </Grid>{" "}
+              <Grid size={{ xs: 12, sm: 6, xl: 3 }}>
+                <ControlledTextField
+                  label="Last Name"
+                  fullWidth
+                  control={control}
+                  type="text"
+                  name="owner.last_name"
+                  errorMessage={errors.owner?.last_name?.message}
+                />
+              </Grid>{" "}
+              <Grid size={{ xs: 12, sm: 6, xl: 3 }}>
+                <ControlledTextField
+                  label="First Name (Secondary)"
+                  fullWidth
+                  control={control}
+                  type="text"
+                  name="owner.second_first_name"
+                  errorMessage={errors.owner?.second_first_name?.message}
+                />
+              </Grid>{" "}
+              <Grid size={{ xs: 12, sm: 6, xl: 3 }}>
+                <ControlledTextField
+                  label="Last Name (Secondary)"
+                  fullWidth
+                  control={control}
+                  type="text"
+                  name="owner.second_last_name"
+                  errorMessage={errors.owner?.second_last_name?.message}
+                />
+              </Grid>{" "}
               <Grid
                 container
-                item
                 spacing={2}
-                xs={12}
+                size={12}
                 sx={{
                   marginLeft: "0rem !important",
                   marginRight: "0rem !important",
                 }}
                 direction={{ xs: "column", sm: "row" }}
               >
-                <Grid item xs={12} sm={6} lg={3}>
+                <Grid size={{ xs: 12, sm: 6, xl: 3 }}>
                   <ControlledPhoneField
                     label="Cell Phone"
                     fullWidth
                     control={control}
                     type="tel"
                     name="owner.cell_phone"
-                    errorMessage={
-                      formState.errors.owner?.cell_phone?.message as string
-                    }
+                    errorMessage={errors.owner?.cell_phone?.message}
                   />
                 </Grid>{" "}
-                <Grid
-                  item
-                  xs={12}
-                  sm={6}
-                  lg={3}
-                  sx={{ paddingLeft: isSmBreakpoint ? "" : "1rem !important" }}
-                >
+                <Grid size={{ xs: 12, sm: 6, xl: 3 }}>
                   <ControlledPhoneField
                     label="Home Phone"
                     fullWidth
                     control={control}
                     type="tel"
                     name="owner.phone"
-                    errorMessage={
-                      formState.errors.owner?.phone?.message as string
-                    }
+                    errorMessage={errors.owner?.phone?.message}
                   />
                 </Grid>{" "}
-                <Grid
-                  item
-                  xs={12}
-                  lg={6}
-                  sx={{ paddingLeft: isLgBreakpoint ? "" : "1rem !important" }}
-                >
+                <Grid size={{ xs: 12, lg: 6 }}>
                   <ControlledEmailField
                     label="Email"
-                    fullWidth
                     control={control}
-                    type="email"
                     name="owner.email"
-                    errorMessage={
-                      formState.errors.owner?.email?.message as string
-                    }
+                    errorMessage={errors.owner?.email?.message}
                   />
                 </Grid>{" "}
               </Grid>{" "}
-              <Grid item xs={12}>
-                <Typography variant="h4">Mailing</Typography>
-              </Grid>
-              <Grid item xs={12}>
-                <TextField label="Address" fullWidth />
-              </Grid>{" "}
-              <Grid item xs={12}>
-                <TextField label="City" fullWidth />
-              </Grid>{" "}
-              <Grid item xs={12}>
-                <TextField label="State" fullWidth />
-              </Grid>{" "}
-              <Grid item xs={12}>
-                <TextField label="Zip Code" fullWidth />
-              </Grid>{" "}
-              <Grid item xs={12}>
+              <Grid size={12}>
                 <Typography variant="h4">Physical</Typography>
               </Grid>
-              <Grid item xs={12}>
-                <TextField label="Address" fullWidth />
+              <Grid size={12}>
+                <ControlledOSMAddressAutocomplete
+                  label="Address"
+                  fullWidth
+                  control={control}
+                  name="owner.physical_address"
+                  errorMessage={errors.owner?.physical_address?.message}
+                  onAddressSelect={(
+                    selectedAddress: string,
+                    selectedCity: string,
+                    selectedState: string,
+                    selectedZip: string,
+                  ) => {
+                    setAddress(selectedAddress);
+                    setCity(selectedCity);
+                    setState(selectedState);
+                    setZip(selectedZip);
+                  }}
+                />
               </Grid>{" "}
-              <Grid item xs={12}>
-                <TextField label="City" fullWidth />
+              <Grid size={{ xs: 12, lg: 6 }}>
+                <ControlledTextField
+                  label="City"
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                  fullWidth
+                  type="text"
+                  control={control}
+                  name="owner.physical_city"
+                  errorMessage={errors.owner?.physical_city?.message}
+                />
               </Grid>{" "}
-              <Grid item xs={12}>
-                <TextField label="State" fullWidth />
+              <Grid size={{ xs: 12, lg: 3 }}>
+                <ControlledTextField
+                  label="State"
+                  value={state}
+                  onChange={(e) => setState(e.target.value)}
+                  fullWidth
+                  type="text"
+                  control={control}
+                  name="owner.physical_state"
+                  errorMessage={errors.owner?.physical_state?.message}
+                />
               </Grid>{" "}
-              <Grid item xs={12}>
-                <TextField label="Zip Code" fullWidth />
+              <Grid size={{ xs: 12, lg: 3 }}>
+                <ControlledTextField
+                  label="Zip Code"
+                  value={zip}
+                  onChange={(e) => setZip(e.target.value)}
+                  fullWidth
+                  type="text"
+                  control={control}
+                  name="owner.physical_zip_code"
+                  errorMessage={errors.owner?.physical_zip_code?.message}
+                />
+              </Grid>{" "}
+              <Grid
+                container
+                size={12}
+                alignItems="center"
+                columnGap={1}
+                rowGap={0}
+              >
+                <Grid size={{ xs: 12, sm: "auto" }}>
+                  <Typography variant="h4">Mailing </Typography>
+                </Grid>
+                <Grid size={{ xs: 12, sm: "grow" }}>
+                  <Typography variant="body1">
+                    (if different from physical address)
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid size={12}>
+                <ControlledTextField
+                  label="Address"
+                  fullWidth
+                  type="text"
+                  control={control}
+                  name="owner.mailing_address"
+                  errorMessage={errors.owner?.mailing_address?.message}
+                />
+              </Grid>{" "}
+              <Grid size={{ xs: 12, lg: 6 }}>
+                <ControlledTextField
+                  label="City"
+                  fullWidth
+                  type="text"
+                  control={control}
+                  name="owner.mail_city"
+                  errorMessage={errors.owner?.mail_city?.message}
+                />
+              </Grid>{" "}
+              <Grid size={{ xs: 12, lg: 3 }}>
+                <ControlledTextField
+                  label="State"
+                  fullWidth
+                  type="text"
+                  control={control}
+                  name="owner.mail_state"
+                  errorMessage={errors.owner?.mail_state?.message}
+                />
+              </Grid>{" "}
+              <Grid size={{ xs: 12, lg: 3 }}>
+                <ControlledTextField
+                  label="Zip Code"
+                  fullWidth
+                  type="text"
+                  control={control}
+                  name="owner.mail_zip_code"
+                  errorMessage={errors.owner?.mail_zip_code?.message}
+                />
               </Grid>{" "}
             </Grid>
-          </Grid>
+            <Grid size={12}>
+              <Typography variant="h2">Location</Typography>
+            </Grid>{" "}
+            <Grid size={{ xs: 12, sm: 6, lg: 4 }}>
+              <ControlledTextField
+                label="Site Name"
+                fullWidth
+                control={control}
+                type="text"
+                name="location.site_name"
+              />
+            </Grid>{" "}
+            <Grid size={12}>
+              <Typography variant="h2">Well</Typography>
+            </Grid>
+            <Grid container spacing={2} direction={{ xs: "column", sm: "row" }}>
+              <Grid size={{ xs: 12, sm: 6, lg: 3 }}>
+                {/* All the statuses are from end point show user the meaning and send the backend the list of codes */}
+                {/* This should be a dropdown multi select */}
+                <ControlledTextField
+                  label="Monitoring status"
+                  fullWidth
+                  control={control}
+                  type="text"
+                  name="well.monitoring_status"
+                />
+              </Grid>{" "}
+              {/* altitude (let users enter in whatever units they want but always return the back in feet) */}
+            </Grid>{" "}
+            <Grid
+              container
+              size={12}
+              justifyContent="space-between"
+              alignItems="center"
+              spacing={2}
+              sx={{ paddingTop: "3rem", paddingBottom: "1rem" }}
+            >
+              <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3, xl: 2 }}>
+                <Button
+                  type="button"
+                  variant="outlined"
+                  color="secondary"
+                  fullWidth
+                  onClick={() => reset()}
+                >
+                  Reset
+                </Button>
+              </Grid>{" "}
+              <Grid size={{ xs: 12, sm: 6, md: 4, lg: 3, xl: 2 }}>
+                <Button type="submit" variant="contained" fullWidth>
+                  Submit
+                </Button>
+              </Grid>{" "}
+            </Grid>{" "}
+          </Grid>{" "}
         </Box>
       </CardContent>
     </Card>
