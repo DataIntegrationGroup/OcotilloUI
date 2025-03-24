1efdc29ed2d35082b683d864a410b07b2c8e09f9
jross <jake.ross@nmt.edu>
2025-03-24 10:35:05 -0600

    added base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,25 @@
-import { defineConfig, loadEnv } from "vite";
+import {defineConfig, loadEnv} from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd());
 
-  return {
-    plugins: [react(), tsconfigPaths()],
-    optimizeDeps: {
-      include: ["@emotion/react", "@emotion/styled", "@mui/material/Tooltip"],
-    },
-    server: {
-      proxy: {
-        "/api": {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path.replace(/^\/api/, ""),
+    return {
+        base: env.VITE_BASE_URL,
+        plugins: [react(), tsconfigPaths()],
+        optimizeDeps: {
+            include: ["@emotion/react", "@emotion/styled", "@mui/material/Tooltip"],
         },
-      },
-    },
-  };
+        server: {
+            proxy: {
+                "/api": {
+                    target: env.VITE_API_URL,
+                    changeOrigin: true,
+                    secure: false,
+                    rewrite: (path) => path.replace(/^\/api/, ""),
+                },
+            },
+        },
+    };
 });
