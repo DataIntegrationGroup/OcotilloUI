75b356a51c8e95a753756acb9766a2ecaead3a93
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2025-04-15 14:28:52 -0500

    [useAll] Rm code comments

--- src/useAll.ts
+++ src/useAll.ts
@@ -1,139 +1,120 @@
-// ===============================================================================
-// Copyright 2024 Jake Ross
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// ===============================================================================
-
-import {useState} from "react";
-import {pickDataProvider, pickNotDeprecated, useDataProvider, useMeta, useResource} from "@refinedev/core";
+import { useState } from "react";
+import {
+  pickDataProvider,
+  useDataProvider,
+  useMeta,
+  useResource,
+} from "@refinedev/core";
 
 // @ts-ignore,
-import type {BaseRecord, MetaQuery} from "@refinedev/core/src/contexts/data/types";
+import type {
+  BaseRecord,
+  MetaQuery,
+} from "@refinedev/core/src/contexts/data/types";
 
 type UseAllOptionsType<
-    TData extends BaseRecord = BaseRecord,
-    TVariables = any
+  TData extends BaseRecord = BaseRecord,
+  TVariables = any,
 > = {
-    /**
-     * Resource name for API data interactions
-     * @default Resource name that it reads from route
-     */
-    resource?: string;
-    /**
-     * Maximum number of items to fetch
-     */
-    maxItemCount?: number;
-    /**
-     * Number of items to fetch per page
-     */
-    pageSize?: number;
-    /**
-     * Data provider name to use for fetching data
-     */
-    dataProviderName?: string;
-    meta?: MetaQuery;
-}
+  /**
+   * Resource name for API data interactions
+   * @default Resource name that it reads from route
+   */
+  resource?: string;
+  /**
+   * Maximum number of items to fetch
+   */
+  maxItemCount?: number;
+  /**
+   * Number of items to fetch per page
+   */
+  pageSize?: number;
+  /**
+   * Data provider name to use for fetching data
+   */
+  dataProviderName?: string;
+  meta?: MetaQuery;
+};
 
 type UseAllReturnType<TData> = {
-    isLoading: boolean;
-    triggerAll: (props?: any) => Promise<any[]>;
-}
+  isLoading: boolean;
+  triggerAll: (props?: any) => Promise<any[]>;
+};
 
 export const useAll = <
-    TData extends BaseRecord = BaseRecord,
-    TVariables = any,
+  TData extends BaseRecord = BaseRecord,
+  TVariables = any,
 >({
-      maxItemCount,
-      pageSize = 500,
-      resource: resourceFromProps,
-      dataProviderName,
-      meta
-  }: UseAllOptionsType<TData, TVariables> = {}): UseAllReturnType<TData> => {
-    const [isLoading, setIsLoading] = useState(false);
+  maxItemCount,
+  pageSize = 500,
+  resource: resourceFromProps,
+  dataProviderName,
+  meta,
+}: UseAllOptionsType<TData, TVariables> = {}): UseAllReturnType<TData> => {
+  const [isLoading, setIsLoading] = useState(false);
 
-    const dataProvider = useDataProvider();
-    const getMeta = useMeta();
-    const {resource, resources, identifier} = useResource(resourceFromProps);
-    const {getList} = dataProvider(
-        pickDataProvider(identifier, dataProviderName, resources),
-    );
-    const combinedMeta = getMeta({
-        resource,
-        meta
-    });
+  const dataProvider = useDataProvider();
+  const getMeta = useMeta();
+  const { resource, resources, identifier } = useResource(resourceFromProps);
+  const { getList } = dataProvider(
+    pickDataProvider(identifier, dataProviderName, resources),
+  );
+  const combinedMeta = getMeta({
+    resource,
+    meta,
+  });
 
-    const triggerAll = async (props) => {
-        setIsLoading(true)
+  const triggerAll = async (props) => {
+    setIsLoading(true);
 
-        let rawData = [];
+    let rawData = [];
 
-        let current = 1;
-        let preparingData = true;
+    let current = 1;
+    let preparingData = true;
 
-        let resourceTag = resource?.name;
-        if (props){
-            resourceTag = props.resource ?? resource?.name;
+    let resourceTag = resource?.name;
+    if (props) {
+      resourceTag = props.resource ?? resource?.name;
+    }
+
+    while (preparingData) {
+      try {
+        const { data, total } = await getList({
+          resource: resourceTag.toString(),
+          pagination: {
+            current,
+            pageSize: pageSize,
+          },
+          meta: combinedMeta,
+        });
+
+        current++;
+
+        rawData.push(...data);
+
+        if (maxItemCount && rawData.length >= maxItemCount) {
+          rawData = rawData.slice(0, maxItemCount);
+          preparingData = false;
         }
 
-        while (preparingData) {
-            try {
-                const {data, total} = await getList({
-                    resource: resourceTag.toString(),
-                    // resource: resource?.name ?? "",
-                    // filters,
-                    // sort: pickNotDeprecated(sorters, sorter),
-                    // sorters: pickNotDeprecated(sorters, sorter),
-                    pagination: {
-                        current,
-                        pageSize: pageSize,
-                    },
-                    meta: combinedMeta,
-                    // metaData: combinedMeta,
-                });
-
-                current++;
-
-                rawData.push(...data);
-
-                if (maxItemCount && rawData.length >= maxItemCount) {
-                    rawData = rawData.slice(0, maxItemCount);
-                    preparingData = false;
-                }
-
-                if (total === rawData.length || data.length < pageSize) {
-                    preparingData = false;
-                }
-
-                if (preparingData === false) {
-                    // setData(rawData);
-                    setIsLoading(false);
-                    return rawData;
-                }
-
-
-            } catch (error) {
-                setIsLoading(false);
-                preparingData = false;
-                console.log('error', error)
-                // onError?.(error);
-
-                return;
-            }
+        if (total === rawData.length || data.length < pageSize) {
+          preparingData = false;
         }
+
+        if (preparingData === false) {
+          setIsLoading(false);
+          return rawData;
+        }
+      } catch (error) {
+        setIsLoading(false);
+        preparingData = false;
+
+        return;
+      }
     }
-    return {
-        isLoading,
-        triggerAll,
-    }
-}
-// ============= EOF =============================================
+  };
+  return {
+    isLoading,
+    triggerAll,
+  };
+};
