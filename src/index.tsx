7e0325e3c0fa1d20679f183dff5428c43b1c1166
jross <jake.ross@nmt.edu>
2025-05-07 14:26:35 -0600

    added sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,26 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
 
-import App from "./App";
+import App from './App'
 
-const container = document.getElementById("root");
+import * as Sentry from '@sentry/react'
+
+let dsn = import.meta.env.VITE_SENTRY_DSN
+
+if (dsn) {
+  Sentry.init({
+    dsn: dsn,
+    // Setting this option to true will send default PII data to Sentry.
+    // For example, automatic IP address collection on events
+    sendDefaultPii: true,
+  })
+}
+
+const container = document.getElementById('root')
 // eslint-disable-next-line
-const root = createRoot(container!);
+const root = createRoot(container!)
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-);
+  </React.StrictMode>
+)
