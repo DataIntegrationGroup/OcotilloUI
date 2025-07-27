8429d6da09760ede505ace141a76737b7852c187
jakeross <jirhiker@gmail.com>
2025-07-27 02:31:00 -0600

    refactor: use dataprovider.custom to upload asset

--- src/pages/dataforge/asset/create.tsx
+++ src/pages/dataforge/asset/create.tsx
@@ -1,4 +1,9 @@
-import { HttpError, useApiUrl } from '@refinedev/core'
+import {
+  HttpError,
+  useApiUrl,
+  useCreate,
+  useDataProvider,
+} from '@refinedev/core'
 import { Create, useAutocomplete } from '@refinedev/mui'
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
@@ -26,7 +31,6 @@ export const AssetCreate: React.FC = () => {
     watch,
   } = useForm<IAsset, HttpError, Nullable<IAsset>>()
   const [isUploadLoading, setIsUploadLoading] = useState(false)
-  const apiUrl = useApiUrl()
 
   const [thingValue, setThingValue] = useState<IThing | null>(null)
   const { autocompleteProps } = useAutocomplete<IThing>({
@@ -34,54 +38,62 @@ export const AssetCreate: React.FC = () => {
     dataProviderName: 'dataforge',
   })
 
+  const dataProvider = useDataProvider()
+  const provider = dataProvider('dataforge')
+
   const onChangeHandler = async (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
     try {
       setIsUploadLoading(true)
 
-      const formData = new FormData()
-
       const target = event.target
       const file: File = (target.files as FileList)[0]
 
+      const formData = new FormData()
       formData.append('file', file)
 
-      const res = await axios.post<{
-        storage_path: string
-        url: string
-      }>(`${apiUrl}/asset/upload`, formData, {
-        withCredentials: false,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-      })
+      const asset = await provider
+        .custom({
+          url: 'asset/upload',
+          method: 'post',
+          payload: formData,
+          headers: {
+            'Content-Type': file.type,
+          },
+        })
+        .then((res) => {
+          if (res.data) {
+            return res.data
+          } else {
+            throw new Error('Upload failed')
+          }
+        })
+        .catch((error) => {
+          setError('file', { message: error.message })
+          setIsUploadLoading(false)
+          throw error
+        })
 
       const { name, size, type, lastModified } = file
-
-      // setAssetValue({
-      //   name,
-      //   size,
-      //   mimetype: type,
-      //   storage_path: res.data.url,
-      // })
       const imagePayload = [
         {
           name,
           size,
           type,
           lastModified,
-          url: res.data.url,
+          url: asset.url,
         },
       ]
       setValue('file', imagePayload, { shouldValidate: true })
       setValue('name', name, { shouldValidate: true })
-      setValue('storage_path', res.data.storage_path, { shouldValidate: true })
+      setValue('storage_path', asset.storage_path, { shouldValidate: true })
       setValue('mime_type', type, { shouldValidate: true })
       setValue('size', size, { shouldValidate: true })
-      setValue('url', res.data.url, { shouldValidate: true })
+      setValue('url', asset.url, { shouldValidate: true })
       setIsUploadLoading(false)
     } catch (error) {
+      console.log(error)
       setError('file', { message: 'Upload failed. Please try again.' })
       setIsUploadLoading(false)
     }
