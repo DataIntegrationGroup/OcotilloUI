ec96eda5d084d8dc333c213611f1198bed02c5b9
Chase Martin <chasetmartin@gmail.com>
2025-07-28 15:32:21 -0700

    fix: update well inventory interface to array of contacts

--- src/interfaces/dataforge/IWellInventoryForm.ts
+++ src/interfaces/dataforge/IWellInventoryForm.ts
@@ -14,48 +14,26 @@ export interface IWellInventoryForm {
     well_type: string
     notes?: string
   }
-  contacts: {
-    primary: {
-      name: string
-      role: string
-      emails?: Array<{
-        email: string
-        email_type: string
-      }>
-      phones?: Array<{
-        phone_number: string
-        phone_type: string
-      }>
-      addresses?: Array<{
-        address_line_1: string
-        address_line_2?: string
-        city: string
-        state: string
-        postal_code: string
-        address_type: string
-      }>
-    }
-    secondary?: {
-      name: string
-      role: string
-      emails?: Array<{
-        email: string
-        email_type: string
-      }>
-      phones?: Array<{
-        phone_number: string
-        phone_type: string
-      }>
-      addresses?: Array<{
-        address_line_1: string
-        address_line_2?: string
-        city: string
-        state: string
-        postal_code: string
-        address_type: string
-      }>
-    }
-  }
+  contacts: Array<{
+    name: string
+    role: string
+    emails?: Array<{
+      email: string
+      email_type: string
+    }>
+    phones?: Array<{
+      phone_number: string
+      phone_type: string
+    }>
+    addresses?: Array<{
+      address_line_1: string
+      address_line_2?: string
+      city: string
+      state: string
+      postal_code: string
+      address_type: string
+    }>
+  }>
   assets?: Array<{
     label: string
     name: string
