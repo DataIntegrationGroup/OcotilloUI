cdbfd3f3194cdbcdd57eff1201b9152d7fce7306
jakeross <jirhiker@gmail.com>
2025-07-28 23:53:41 -0600

    feat: added map to water level form

--- src/interfaces/dataforge/IGroundwaterLevel.ts
+++ src/interfaces/dataforge/IGroundwaterLevel.ts
@@ -4,7 +4,8 @@ export interface IGroundwaterLevel {
 
 export interface IGroundwaterLevelForm {
   thing_id: number
-  observation_timestamp: string
+  series_id: number
+  observation_timestamp: Date
   depth_to_water: number
   measuring_point_height: number
   release_status: string
--- src/pages/dataforge/groundwaterlevelform/index.tsx
+++ src/pages/dataforge/groundwaterlevelform/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import Box from '@mui/material/Box'
 import { useForm } from '@refinedev/react-hook-form'
 import { HttpError } from '@refinedev/core'
@@ -9,7 +9,15 @@ import { DateTimePicker } from '@mui/x-date-pickers'
 import { Controller } from 'react-hook-form'
 import Autocomplete from '@mui/material/Autocomplete'
 import { IThing } from '@/interfaces/dataforge/IThing'
+import { ISeries } from '@/interfaces/dataforge/ISeries'
+import { ILexicon } from '@/interfaces/dataforge/ILexicon'
 import dayjs from 'dayjs'
+
+import { MapComponent } from '@/components'
+import { Layer, Source } from 'react-map-gl'
+import { MapRef } from 'react-map-gl'
+import { Button, Stack } from '@mui/material'
+import { MapOutlined } from '@mui/icons-material'
 
 export const GroundwaterLevelForm: React.FC = () => {
   const {
@@ -34,13 +42,14 @@ export const GroundwaterLevelForm: React.FC = () => {
     },
   })
 
-  const { autocompleteProps } = useAutocomplete<ILexicon>({
-    resource: 'lexicon',
-    dataProviderName: 'dataforge',
-    meta: {
-      params: { category: 'release_status' },
-    },
-  })
+  const { autocompleteProps: autocompletePropsReleaseStatus } =
+    useAutocomplete<ILexicon>({
+      resource: 'lexicon',
+      dataProviderName: 'dataforge',
+      meta: {
+        params: { category: 'release_status' },
+      },
+    })
 
   const { autocompleteProps: autocompletePropsThing } = useAutocomplete<IThing>(
     {
@@ -56,20 +65,145 @@ export const GroundwaterLevelForm: React.FC = () => {
     }
   )
 
+  const [selectedThingID, setSelectedThingID] = useState<number | null>(null)
+  const [selectedThingFeatureCollection, setSelectedThingFeatureCollection] =
+    useState(null)
+  const [displayMap, setDisplayMap] = useState(false)
+
+  const { autocompleteProps: autocompletePropsSeries } =
+    useAutocomplete<ISeries>({
+      resource: 'series',
+      dataProviderName: 'dataforge',
+      meta: {
+        params: { thing_id: selectedThingID },
+      },
+    })
+
+  const mapRef = useRef<MapRef>(null)
+  useEffect(() => {
+    if (
+      selectedThingFeatureCollection &&
+      selectedThingFeatureCollection.features?.length > 0
+    ) {
+      const coords =
+        selectedThingFeatureCollection.features[0].geometry.coordinates
+
+      if (coords && mapRef.current) {
+        mapRef.current.flyTo({
+          center: coords,
+          zoom: 10, // adjust zoom as needed
+          essential: true,
+        })
+      }
+    }
+  }, [selectedThingFeatureCollection])
+
   return (
     <Create goBack={<></>} saveButtonProps={saveButtonProps}>
       <Box
         component="form"
         sx={{ display: 'flex', flexDirection: 'column', gap: '12px' }}
       >
+        <Stack direction="row" spacing={2}>
+          <Box sx={{ flexGrow: 1 }}>
+            <Controller
+              name="thing_id"
+              control={control}
+              // rules={{ required: 'This field is required' }}
+              render={({ field }) => (
+                <Autocomplete
+                  {...autocompletePropsThing}
+                  // value={thingValue}
+                  onChange={(_, newValue) => {
+                    if (newValue === null) {
+                      setSelectedThingFeatureCollection({
+                        type: 'FeatureCollection',
+                        features: [],
+                      })
+                    } else if (newValue?.geometry === null) {
+                      setSelectedThingFeatureCollection({
+                        type: 'FeatureCollection',
+                        features: [],
+                      })
+                    } else {
+                      setSelectedThingFeatureCollection({
+                        type: 'FeatureCollection',
+                        features: [
+                          {
+                            type: 'Feature',
+                            id: newValue?.id || null,
+                            geometry: newValue?.geometry,
+                          },
+                        ],
+                      })
+                    }
+                    setSelectedThingID(newValue?.id || null)
+                    field.onChange(newValue?.id || null)
+                  }}
+                  // options={['Draft', 'Public', 'Private']}
+                  getOptionKey={(option) => option.id}
+                  getOptionLabel={(option) =>
+                    `${option.name}: (${option.id})` || ''
+                  }
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      label="Thing"
+                      margin="normal"
+                      error={!!errors.thing_id}
+                      helperText={errors.thing_id?.message}
+                    />
+                  )}
+                />
+              )}
+            />
+          </Box>
+          <Box sx={{ alignItems: 'center', display: 'flex' }}>
+            <Button
+              sx={{ height: '48px' }}
+              variant="outlined"
+              startIcon={<MapOutlined />}
+              // variant="contained"
+              onClick={() => {
+                setDisplayMap((prev) => !prev)
+              }}
+            />
+          </Box>
+        </Stack>
+
+        <Box sx={{ paddingLeft: '50px', paddingRight: '50px' }}>
+          {displayMap && (
+            <MapComponent mapRef={mapRef}>
+              <Source
+                key="selectedThing"
+                id="selectedThing"
+                type="geojson"
+                data={selectedThingFeatureCollection}
+              >
+                <Layer
+                  id="location"
+                  type="circle"
+                  paint={{
+                    'circle-radius': 6,
+                    'circle-color': '#B42222',
+                    'circle-stroke-color': '#ffffff',
+                    'circle-stroke-width': 1,
+                  }}
+                />
+              </Source>
+            </MapComponent>
+          )}
+        </Box>
+
         <Controller
-          name="thing_id"
+          name="series_id"
           control={control}
           // rules={{ required: 'This field is required' }}
           render={({ field }) => (
             <Autocomplete
-              {...autocompletePropsThing}
+              {...autocompletePropsSeries}
               // value={thingValue}
+              disabled={selectedThingID === null}
               onChange={(_, newValue) => {
                 // setThingValue(newValue)
                 field.onChange(newValue?.id || null)
@@ -80,10 +214,10 @@ export const GroundwaterLevelForm: React.FC = () => {
               renderInput={(params) => (
                 <TextField
                   {...params}
-                  label="Thing"
+                  label="Series"
                   margin="normal"
-                  error={!!errors.thing_id}
-                  helperText={errors.thing_id?.message}
+                  error={!!errors.series_id}
+                  helperText={errors.series_id?.message}
                 />
               )}
             />
@@ -138,7 +272,7 @@ export const GroundwaterLevelForm: React.FC = () => {
           rules={{ required: 'This field is required' }}
           render={({ field }) => (
             <Autocomplete
-              {...autocompleteProps}
+              {...autocompletePropsReleaseStatus}
               // value={thingValue}
               onChange={(_, newValue) => {
                 // setThingValue(newValue)
