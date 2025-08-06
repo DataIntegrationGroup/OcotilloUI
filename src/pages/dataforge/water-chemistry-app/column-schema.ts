9b97687676e18874aa98011318683016850dce9e
jakeross <jirhiker@gmail.com>
2025-08-06 14:43:32 -0600

    feat: started water chemistry upload app

---
+++ src/pages/dataforge/water-chemistry-app/column-schema.ts
@@ -0,0 +1,35 @@
+export const ID_COL = 8
+export const SAMPLE_DATE_COL = 20
+export const PARAMETER_COL = 3
+export const RESULT_UNITS_COL = 4
+export const RESULT_COL = 16
+
+// 0: "CustomerName"
+// 1: "CustomerContact"
+// 2: "PhoneNumber"
+// 3: "Param"
+// 4: "Results_Units"
+// 5: "Dilution"
+// 6: "AnalysisTime"
+// 7: "SampleNumber"
+// 8: "CustomerSampleNumber"
+// 9: "SamplePointID"
+// 10: "OrderID"
+// 11: "ProjectID"
+// 12: "Matrix"
+// 13: "Method"
+// 14: "Test"
+// 15: "UpperLimit"
+// 16: "ReportedND"
+// 17: "ReceiveDate"
+// 18: "LowerLimit"
+// 19: "LowerXdilution"
+// 20: "SampleDate"
+
+export interface IUploadSchema {
+  id: number
+  sampleDate: string
+  parameter: string
+  resultUnits: string
+  result: string
+}
--- src/pages/dataforge/water-chemistry-app/index.tsx
+++ src/pages/dataforge/water-chemistry-app/index.tsx
@@ -1,17 +1,32 @@
-import { Card, Box } from '@mui/material'
+import { Card, Box, Button, Input, Typography } from '@mui/material'
 import { useImport } from '@refinedev/core'
 import { useState } from 'react'
 import { ImportButton } from '@refinedev/mui'
+import FileUploadIcon from '@mui/icons-material/FileUpload'
+import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid'
+import { date } from 'yup'
+import {
+  ID_COL,
+  IUploadSchema,
+  PARAMETER_COL,
+  RESULT_COL,
+  RESULT_UNITS_COL,
+  SAMPLE_DATE_COL,
+} from '@/pages/dataforge/water-chemistry-app/column-schema'
 
-interface IChemUpload {}
+interface IChemUpload {
+  sampleDate: string
+}
 
 interface EntryProps {}
 
 export const WaterChemistryApp: React.FC<EntryProps> = () => {
+  const [isUploadLoading, setIsUploadLoading] = useState(false)
   const [importProgress, setImportProgress] = useState({
     processed: 0,
     total: 0,
   })
+  const [rows, setRows] = useState<IUploadSchema[]>()
 
   const { inputProps, isLoading } = useImport<IChemUpload>({
     resource: 'water-chemistry-app',
@@ -27,21 +42,135 @@ export const WaterChemistryApp: React.FC<EntryProps> = () => {
     },
   })
 
+  const onChangeHandler = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    try {
+      setIsUploadLoading(true)
+
+      const target = event.target
+      const file: File = (target.files as FileList)[0]
+
+      console.log(file)
+      //parse each line of the file and create an array of objects
+      const fileContent = await file.text()
+      const lines = fileContent.split('\n')
+      const header = lines[0].split(',')
+      console.log(header)
+
+      lines.shift()
+      const nrows = lines.map((line, index) => {
+        const row = line.split(',')
+        return {
+          // id: row[ID_COL],
+          id: index + 1,
+          parameter: row[PARAMETER_COL],
+          resultUnits: row[RESULT_UNITS_COL],
+          result: row[RESULT_COL],
+          sampleDate: row[SAMPLE_DATE_COL],
+        }
+      })
+
+      setRows(nrows)
+      setIsUploadLoading(false)
+    } catch (error) {
+      console.error('Error uploading file:', error)
+      setIsUploadLoading(false)
+    }
+  }
+
+  const columns: GridColDef[] = [
+    {
+      field: 'id',
+      headerName: 'ID',
+      width: 90,
+      type: 'string',
+    },
+    {
+      field: 'sampleDate',
+      headerName: 'Sample Date',
+      width: 150,
+      type: 'string',
+    },
+    {
+      field: 'parameter',
+      headerName: 'Parameter',
+      width: 150,
+      type: 'string',
+    },
+    {
+      field: 'resultUnits',
+      headerName: 'Result Units',
+      width: 150,
+      type: 'string',
+    },
+    {
+      field: 'result',
+      headerName: 'Result',
+      width: 150,
+      type: 'string',
+    },
+    // { field: 'id', headerName: 'ID', width: 90 },
+    // { field: 'name', headerName: 'Name', width: 150 },
+    // { field: 'value', headerName: 'Value', width: 150 },
+    // Add more columns as needed
+  ]
+
   return (
     <Box>
-      <Card sx={{ p: 3 }}>
+      <Card sx={{ p: 2, m: 2 }}>
         <h1>Water Chemistry App</h1>
         <p> Use this app to upload data from a lab generated CSV file.</p>
         <p>Stay tuned for more updates!</p>
       </Card>
-      <Card sx={{ p: 3 }}>
-        {isLoading ? (
-          <p>
-            {importProgress.processed} / {importProgress.total}
-          </p>
-        ) : (
-          <p>Import CSV</p>
-        )}
+      {/*<Card sx={{ p: 2, m: 2 }}>*/}
+      {/*  {isLoading ? (*/}
+      {/*    <p>*/}
+      {/*      {importProgress.processed} / {importProgress.total}*/}
+      {/*    </p>*/}
+      {/*  ) : (*/}
+      {/*    <p>Import CSV</p>*/}
+      {/*  )}*/}
+      {/*</Card>*/}
+      <Card sx={{ p: 2, m: 2 }}>
+        <label>
+          <Input
+            id="csv-input"
+            type="file"
+            sx={{ display: 'none' }}
+            onChange={onChangeHandler}
+          />
+          <input id="file" type="hidden" />
+          <Button
+            loading={isUploadLoading}
+            loadingPosition="end"
+            endIcon={<FileUploadIcon />}
+            variant="contained"
+            component="span"
+          >
+            Upload
+          </Button>
+          <br />
+        </label>
+        {/*{errors.file && (*/}
+        {/*  <Typography variant="caption" color="#fa541c">*/}
+        {/*    {errors.file?.message?.toString()}*/}
+        {/*  </Typography>*/}
+        {/*)}*/}
+        {/*</label>*/}
+      </Card>
+
+      <Card sx={{ p: 2, m: 2 }}>
+        <DataGrid
+          initialState={{
+            pagination: { paginationModel: { pageSize: 5 } },
+          }}
+          pageSizeOptions={[5]}
+          rows={rows}
+          columns={columns}
+        />
+      </Card>
+      <Card sx={{ p: 2, m: 2 }}>
         <ImportButton inputProps={inputProps} />
       </Card>
     </Box>
