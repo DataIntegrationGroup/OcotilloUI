8451c5ccd7f0378ec90472a4b46a63ed5621215d
jakeross <jirhiker@gmail.com>
2025-08-13 15:32:43 -0600

    refactor: rebrand from Data Manager/DataForge to Ocotillo

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -18,18 +18,33 @@ import {
   Stack,
   Typography,
   useTheme,
+  Box,
 } from '@mui/material'
+
+import ocotilloImage from '@/img/ocotillo.png'
+import React from 'react'
 
 export const Home = () => {
   const theme = useTheme()
 
   return (
     <Card>
-      <CardHeader title="Home" />
       <CardContent>
         <Stack spacing={2}>
-          <Typography variant="h1">Welcome to Ocotillo.</Typography>
-          <Typography variant="h2">NMBGMR's Data Management Portal</Typography>
+          <Box
+            sx={{
+              backgroundImage: `url(${ocotilloImage})`,
+              backgroundRepeat: 'no-repeat',
+              backgroundSize: 'cover',
+              height: '500px',
+              padding: 2,
+            }}
+          >
+            <Typography variant="h1">Welcome to Ocotillo.</Typography>
+            <Typography variant="h2">
+              NMBGMR's Data Management Portal
+            </Typography>
+          </Box>
           <List
             sx={{ width: '100%', maxWidth: 300 }}
             subheader={
@@ -40,11 +55,16 @@ export const Home = () => {
           >
             <ListItem>
               <ListItemAvatar>
-                <Avatar>
+                <Avatar sx={{ bgcolor: theme.palette.secondary.main }}>
                   <StorageOutlined />
                 </Avatar>
               </ListItemAvatar>
-              <ListItemText primary="NM_Aquifer" />
+              <ListItemText
+                primary="Aquifer Mapping Program"
+                sx={{
+                  color: theme.palette.secondary.main,
+                }}
+              />
             </ListItem>
             <ListItemButton
               component="a"
@@ -64,11 +84,14 @@ export const Home = () => {
             </ListItemButton>
             <ListItem>
               <ListItemAvatar>
-                <Avatar>
+                <Avatar sx={{ bgcolor: theme.palette.secondary.main }}>
                   <Plumbing />
                 </Avatar>
               </ListItemAvatar>
-              <ListItemText primary="NM_Wells" />
+              <ListItemText
+                primary="Geothermal Program"
+                sx={{ color: theme.palette.secondary.main }}
+              />
             </ListItem>
             <ListItemButton
               component="a"
