482de42c4427094140a4d83050cd91e763e4977d
jakeross <jirhiker@gmail.com>
2025-08-16 15:55:53 -0600

    feat: started map polygon component

---
+++ src/components/MapPolygonComponent.tsx
@@ -0,0 +1,31 @@
+import Grid from '@mui/material/Grid2'
+import MapComponent from '@/components/MapComponent'
+
+interface MapPolygonComponentProps {
+  mapRef: any
+  polygon: any
+  setPolygon: (any) => void
+}
+
+export const MapPolygonComponent: React.FC<MapPolygonComponentProps> = ({
+  mapRef,
+  polygon,
+  setPolygon,
+}) => {
+  return (
+    <Grid container spacing={2} alignItems="center">
+      <Grid size={12}>
+        <MapComponent
+          mapRef={mapRef}
+          style={{ height: '600px', width: '100%' }}
+          initialViewState={{
+            longitude: -106.4,
+            latitude: 34.5,
+            zoom: 6,
+          }}
+          setSelectionPolygons={setPolygon}
+        />
+      </Grid>
+    </Grid>
+  )
+}
