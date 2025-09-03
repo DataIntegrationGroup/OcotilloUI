666295484b42898bb0f02ab689a5597cbd727392
Chase Martin <chasetmartin@gmail.com>
2025-09-03 09:44:20 -0700

    feat: update sensor fields and add release status

--- src/interfaces/ocotillo/ISensor.ts
+++ src/interfaces/ocotillo/ISensor.ts
@@ -3,8 +3,10 @@ export interface ISensor {
   name: string
   model?: string | null
   serial_no?: string | null
-  date_installed?: string | null
-  date_removed?: string | null
+  datetime_installed?: string | null
+  datetime_removed?: string | null
+  recording_interval?: number | null
   notes?: string | null
   created_at: Date
+  release_status: string
 }
