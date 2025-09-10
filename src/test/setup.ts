f02edc7ae111dfed693c0c6bd4f8a7d44a758e19
Chase Martin <chasetmartin@gmail.com>
2025-09-10 08:45:28 -0700

    refactor: add node env and api url check to test setup.ts file to double check environment before tests

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -2,6 +2,7 @@ import { expect, afterEach, beforeAll, vi } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import * as matchers from '@testing-library/jest-dom/matchers'
 import { checkMockServerHealth } from './mock-server'
+import { ocotilloDataProvider } from '@/providers/ocotillo-data-provider'
 
 process.env.NODE_ENV = 'test'
 
@@ -25,6 +26,19 @@ Object.defineProperty(global, 'localStorage', {
 
 // Global test setup
 beforeAll(async () => {
+
+    //double check node env is test
+    if (process.env.NODE_ENV !== 'test') {
+      throw new Error('Non-Test environment detected. Tests must be run with NODE_ENV=test')
+    }
+
+    //check ocotillo data provider is using the mock server
+    if (ocotilloDataProvider.getApiUrl() !== 'http://127.0.0.1:4010') {
+      throw new Error('Ocotillo data provider is not using the mock server. Tests must be run with NODE_ENV=test')
+    } else {
+      console.log('Ocotillo data provider is using the mock server, running tests...')
+    }
+
     const isHealthy = await checkMockServerHealth()
     if (!isHealthy) {
       console.warn('Mock server health check failed. Some integration tests may fail.')
