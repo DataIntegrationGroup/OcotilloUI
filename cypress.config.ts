c6df7789dcc30ee832dd82c0367acb51ad9e7fb1
Chase Martin <chasetmartin@gmail.com>
2025-09-24 14:40:36 -0700

    fix: point cypress and localhost:4173 to run with npm start per cypress CI docs

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
   },
 
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'http://localhost:4173',
     setupNodeEvents(on, config) {
       process.env.NODE_ENV = 'test'
     },
