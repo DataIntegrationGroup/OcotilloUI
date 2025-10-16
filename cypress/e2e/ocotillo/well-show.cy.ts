2eadf8555aee59cff3230703290ec7eb173dc142
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2025-10-16 14:23:46 -0500

    [cypress/e2e] Fix interceptor & data-testid

--- cypress/e2e/ocotillo/well-show.cy.ts
+++ cypress/e2e/ocotillo/well-show.cy.ts
@@ -4,7 +4,7 @@ describe('Thing Well Show Page', () => {
   beforeEach(() => {
     cy.login()
 
-    cy.intercept('GET', '**/thing/107').as('getWell')
+    cy.intercept('GET', '**/thing/**').as('getWell')
     cy.visit('/ocotillo/well/show/107')
     cy.wait('@getWell')
   })
