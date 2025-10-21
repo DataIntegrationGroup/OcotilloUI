8932cb81f2140b73bbfe8fe85e4525e85de30caf
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2025-10-21 10:35:14 -0500

    [CommonColumnDefs] Fix types

--- src/components/CommonColumnDefs.tsx
+++ src/components/CommonColumnDefs.tsx
@@ -1,4 +1,6 @@
+import { Box } from '@mui/material'
 import { DeleteButton, EditButton, ShowButton } from '@refinedev/mui'
+import { GridColDef } from '@mui/x-data-grid'
 
 export const idColumnDef = () => {
   return {
@@ -9,42 +11,42 @@ export const idColumnDef = () => {
   }
 }
 
-export const actionColumnDef = ({ resource = {} }: { resource: any }) => {
-  return {
-    field: 'actions',
-    headerName: 'Actions',
-    renderCell: function render({ row }) {
-      return (
-        <div style={{ display: 'flex', gap: '8px' }}>
-          <EditButton
-            resource={resource}
-            size="small"
-            hideText
-            recordItemId={row.id}
-            variant={'outlined'}
-            sx={{ maxWidth: 50, maxHeight: 5, px: 1 }}
-          />
-          <ShowButton
-            resource={resource}
-            size="small"
-            hideText
-            recordItemId={row.id}
-            variant={'outlined'}
-            sx={{ maxWidth: 50, maxHeight: 5, px: 1 }}
-          />
-          <DeleteButton
-            resource={resource}
-            size="small"
-            hideText
-            recordItemId={row.id}
-            variant={'outlined'}
-            sx={{ maxWidth: 50, maxHeight: 5, px: 1 }}
-          />
-        </div>
-      )
-    },
-    align: 'center',
-    headerAlign: 'center',
-    minWidth: 210,
-  }
-}
+export const actionColumnDef = ({
+  resource,
+}: { resource?: any } = {}): GridColDef => ({
+  field: 'actions',
+  headerName: 'Actions',
+  renderCell: function render({ row }) {
+    return (
+      <Box component="div" sx={{ display: 'flex', gap: '8px' }}>
+        <EditButton
+          resource={resource}
+          size="small"
+          hideText
+          recordItemId={row.id}
+          variant={'outlined'}
+          sx={{ maxWidth: 50, maxHeight: 5, px: 1 }}
+        />
+        <ShowButton
+          resource={resource}
+          size="small"
+          hideText
+          recordItemId={row.id}
+          variant={'outlined'}
+          sx={{ maxWidth: 50, maxHeight: 5, px: 1 }}
+        />
+        <DeleteButton
+          resource={resource}
+          size="small"
+          hideText
+          recordItemId={row.id}
+          variant={'outlined'}
+          sx={{ maxWidth: 50, maxHeight: 5, px: 1 }}
+        />
+      </Box>
+    )
+  },
+  align: 'center',
+  headerAlign: 'center',
+  minWidth: 210,
+})
