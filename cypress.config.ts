89b2b3603b704a22e69ba1911677f02f621e1d1e
Chase Martin <chasetmartin@gmail.com>
2025-10-22 11:07:45 -0700

    feat: update cypress config based on cypress-cucumber quickstart guide

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
-
-const cucumber = require("cypress-cucumber-preprocessor").default;
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
 
 export default defineConfig({
   component: {
@@ -11,16 +12,27 @@ export default defineConfig({
   },
 
   e2e: {
-    baseUrl: 'http://localhost:4173',
+    baseUrl: "http://localhost:4173",
     specPattern: "**/*.feature",
-    setupNodeEvents(on, config) {
-      on("file:preprocessor", cucumber())
-      process.env.NODE_ENV = 'test'
+    async setupNodeEvents(
+      on: Cypress.PluginEvents,
+      config: Cypress.PluginConfigOptions
+    ): Promise<Cypress.PluginConfigOptions> {
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      process.env.NODE_ENV = "test";
+      return config;
     },
     env: {
-      // set test auth variable to true to mock Authentik provider token
       VITE_TEST_AUTH: true,
-      NODE_ENV: 'test',
-    }
+      NODE_ENV: "test",
+    },
   },
 });
