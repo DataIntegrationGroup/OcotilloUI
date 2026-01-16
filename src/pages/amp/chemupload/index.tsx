1398ca9527740f120b03c6b7cb468da47cfacd5a
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2026-01-16 15:30:48 -0600

    [amp/chemupload] Update clean up pg

--- src/pages/amp/chemupload/index.tsx
+++ src/pages/amp/chemupload/index.tsx
@@ -1,50 +1,38 @@
-import Typography from "@mui/material/Typography";
-import {Card} from "@mui/material";
-import Stack from "@mui/material/Stack";
-import {useImport, useShow} from "@refinedev/core";
-import Box from "@mui/material/Box";
-import {Create} from "@refinedev/mui";
-import {useState} from "react";
+import { useState } from 'react'
+import { Card } from '@mui/material'
+import { useImport } from '@refinedev/core'
 
-interface IChemUpload {
+interface IChemUpload {}
 
-}
 export const ChemUpload = () => {
-    // const {query} = useShow({
-    //     resource: 'chemupload',
-    //     id: 'chemupload',
-    //     dataProviderName: 'amp'
-    // });
-    // const stats = query.data?.data
-    // console.log(query.data?.data)
-    const [importProgress, setImportProgress] = useState({
-        processed: 0,
-        total: 0,
-    });
+  const [importProgress, setImportProgress] = useState({
+    processed: 0,
+    total: 0,
+  })
 
-    const { inputProps, isLoading } = useImport<IChemUpload>({
-        resource: "products",
-        onFinish: () => {
-            alert("Import completed!");
-        },
-        onProgress: (progress) => {
-            setImportProgress({
-                processed: progress.processedAmount,
-                total: progress.totalAmount,
-            });
-        },
-    });
+  const { inputProps, isLoading } = useImport<IChemUpload>({
+    resource: 'products',
+    onFinish: () => {
+      alert('Import completed!')
+    },
+    onProgress: (progress) => {
+      setImportProgress({
+        processed: progress.processedAmount,
+        total: progress.totalAmount,
+      })
+    },
+  })
 
-    return (
-        <Card sx={{p: 3}}>
-            {isLoading ? (
-                <p>
-                    {importProgress.processed} / {importProgress.total}
-                </p>
-            ) : (
-                <p>Import CSV</p>
-            )}
-            <input name="csv" {...inputProps} />
-        </Card>
-    )
+  return (
+    <Card sx={{ p: 3 }}>
+      {isLoading ? (
+        <p>
+          {importProgress.processed} / {importProgress.total}
+        </p>
+      ) : (
+        <p>Import CSV</p>
+      )}
+      <input name="csv" {...inputProps} />
+    </Card>
+  )
 }
