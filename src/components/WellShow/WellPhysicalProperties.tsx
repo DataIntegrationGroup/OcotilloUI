83dd906fc74c18f69bc126c89607a5656e19cd56
Tyler Adam Martinez <TylerAdamMartinez@outlook.com>
2026-03-30 13:18:40 -0500

    feat(WellPhysicalProperties): Add Measuring point description

--- src/components/WellShow/WellPhysicalProperties.tsx
+++ src/components/WellShow/WellPhysicalProperties.tsx
@@ -10,55 +10,63 @@ export const WellPhysicalPropertiesAccordion = ({ well }: { well?: IWell }) => {
         </Typography>
       </Box>
       <Box sx={{ p: 2 }}>
-      <Stack spacing={1}>
-        <Box sx={{ display: 'flex', gap: 1, alignItems: 'baseline' }}>
-          <Typography variant="body2" component="span">
-            Casing Diameter:
-          </Typography>
-          <Typography variant="body2" color="text.secondary" component="span">
-            {well?.well_casing_diameter?.toFixed(2) || 'N/A'}
-            {well?.well_casing_diameter
-              ? ` ${well?.well_casing_diameter_unit}`
-              : null}
-          </Typography>
-        </Box>
-        <Box sx={{ display: 'flex', gap: 1, alignItems: 'baseline' }}>
-          <Typography variant="body2" component="span">
-            Casing Depth:
-          </Typography>
-          <Typography variant="body2" color="text.secondary" component="span">
-            {well?.well_casing_depth?.toFixed(2) || 'N/A'}
-            {well?.well_casing_depth
-              ? ` ${well?.well_casing_depth_unit}`
-              : null}
-          </Typography>
-        </Box>
-        <Box sx={{ display: 'flex', gap: 1, alignItems: 'baseline' }}>
-          <Typography variant="body2" component="span">
-            Casing Materials:
-          </Typography>
-          <Typography variant="body2" color="text.secondary" component="span">
-            {well?.well_casing_materials?.join(', ') || 'N/A'}
-          </Typography>
-        </Box>
-        <Box sx={{ display: 'flex', gap: 1, alignItems: 'baseline' }}>
-          <Typography variant="body2" component="span">
-            Pump Type:
-          </Typography>
-          <Typography variant="body2" color="text.secondary" component="span">
-            {well?.well_pump_type || 'N/A'}
-          </Typography>
-        </Box>
-        <Box sx={{ display: 'flex', gap: 1, alignItems: 'baseline' }}>
-          <Typography variant="body2" component="span">
-            Pump Depth:
-          </Typography>
-          <Typography variant="body2" color="text.secondary" component="span">
-            {well?.well_pump_depth?.toFixed(2) || 'N/A'}
-            {well?.well_pump_depth ? ` ${well?.well_pump_depth_unit}` : null}
-          </Typography>
-        </Box>
-      </Stack>
+        <Stack spacing={1}>
+          <Box sx={{ display: 'flex', gap: 1, alignItems: 'baseline' }}>
+            <Typography variant="body2" component="span">
+              Casing Diameter:
+            </Typography>
+            <Typography variant="body2" color="text.secondary" component="span">
+              {well?.well_casing_diameter?.toFixed(2) || 'N/A'}
+              {well?.well_casing_diameter
+                ? ` ${well?.well_casing_diameter_unit}`
+                : null}
+            </Typography>
+          </Box>
+          <Box sx={{ display: 'flex', gap: 1, alignItems: 'baseline' }}>
+            <Typography variant="body2" component="span">
+              Casing Depth:
+            </Typography>
+            <Typography variant="body2" color="text.secondary" component="span">
+              {well?.well_casing_depth?.toFixed(2) || 'N/A'}
+              {well?.well_casing_depth
+                ? ` ${well?.well_casing_depth_unit}`
+                : null}
+            </Typography>
+          </Box>
+          <Box sx={{ display: 'flex', gap: 1, alignItems: 'baseline' }}>
+            <Typography variant="body2" component="span">
+              Casing Materials:
+            </Typography>
+            <Typography variant="body2" color="text.secondary" component="span">
+              {well?.well_casing_materials?.join(', ') || 'N/A'}
+            </Typography>
+          </Box>
+          <Box sx={{ display: 'flex', gap: 1, alignItems: 'baseline' }}>
+            <Typography variant="body2" component="span">
+              Pump Type:
+            </Typography>
+            <Typography variant="body2" color="text.secondary" component="span">
+              {well?.well_pump_type || 'N/A'}
+            </Typography>
+          </Box>
+          <Box sx={{ display: 'flex', gap: 1, alignItems: 'baseline' }}>
+            <Typography variant="body2" component="span">
+              Pump Depth:
+            </Typography>
+            <Typography variant="body2" color="text.secondary" component="span">
+              {well?.well_pump_depth?.toFixed(2) || 'N/A'}
+              {well?.well_pump_depth ? ` ${well?.well_pump_depth_unit}` : null}
+            </Typography>
+          </Box>
+          <Box sx={{ display: 'flex', gap: 1, alignItems: 'baseline' }}>
+            <Typography variant="body2" component="span">
+              Measuring Point:
+            </Typography>
+            <Typography variant="body2" color="text.secondary" component="span">
+              {well?.measuring_point_description || 'N/A'}
+            </Typography>
+          </Box>
+        </Stack>
       </Box>
     </Paper>
   )
